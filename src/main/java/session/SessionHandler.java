d9356e5c4cb17b997b8215be1b87817c3ebc6f1a
Abdul Sattar <asattar@exchangesolutions.com>
2016-06-08 16:43:52 -0400

    code changes

--- src/main/java/session/SessionHandler.java
+++ src/main/java/session/SessionHandler.java
@@ -1,30 +1,67 @@
 package session;
 
+import akka.persistence.*;
 import akka.actor.ReceiveTimeout;
 import akka.actor.UntypedActor;
+import akka.persistence.*;
 import scala.concurrent.duration.Duration;
 
 import javax.mail.Session;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by asattar on 2016-06-08.
- */
-public class SessionHandler extends UntypedActor{
+public class SessionHandler extends UntypedPersistentActor{
+
+    @Override
+    public void onReceiveRecover(Object msg) throws Exception {
+
+        if (msg instanceof SnapshotOffer) {
+            eventCount = (Integer)((SnapshotOffer)msg).snapshot();
+        } else {
+            unhandled(msg);
+        }
+    }
+
+    @Override
+    public void onReceiveCommand(Object msg) throws Exception {
+
+        if (msg instanceof ReceiveTimeout) {
+
+            System.out.println("Timeout received:" + getSelf());
+
+            saveSnapshot(eventCount);
+        }
+        else if (msg instanceof SaveSnapshotSuccess) {
+
+            System.out.println("Save snapshot success:" + ((SaveSnapshotSuccess)msg).metadata());
+
+            getContext().stop(self());
+        }
+
+        else if (msg instanceof SaveSnapshotFailure) {
+
+            System.out.println("Save snapshot success:" + ((SaveSnapshotFailure)msg).metadata());
+
+            saveSnapshot(eventCount);
+        }
+
+        else {
+            eventCount++;
+
+            saveSnapshot(eventCount);
+
+            System.out.println("SessionHandler:" + msg);
+        }
+    }
+
+    @Override
+    public String persistenceId() {
+        return getSelf().path().name();
+    }
+
+    public Integer eventCount = 0;
 
     public SessionHandler() {
         getContext().setReceiveTimeout(Duration.create(15, TimeUnit.SECONDS));
     }
-    @Override
-    public void onReceive(Object message) throws Exception {
 
-        if (message instanceof ReceiveTimeout) {
-
-            System.out.println("Timeout received:" + getSelf());
-
-            context().stop(self());
-        }
-
-        System.out.println("SessionHandler:" + message);
-    }
 }
